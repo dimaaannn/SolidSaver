c0555cade357023e82e94f6ec3b04b8aa5decf07
dimaaannn <Dk>
2020-12-01 14:23:01 +0300

    Property moved to separated file

---
+++ SWAPIlib/Properties.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SWAPIlib
+{
+    /// <summary>
+    /// Возможность доступа к свойству
+    /// </summary>
+    /// <param name="appModel"></param>
+    /// <returns></returns>
+    public delegate bool PropValidator(AppModel appModel);
+
+    /// <summary>
+    /// Конвертер свойств в нужный формат
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
+    /// <param name="outvalue"></param>
+    /// <returns></returns>
+    public delegate bool PropValueConvertor<T>(T value, out T outvalue);
+
+    public static class PropValidatorTemplate
+    {
+
+        /// <summary>
+        /// Шаблон - является деталью или сборкой
+        /// </summary>
+        /// <param name="appModel"></param>
+        /// <returns></returns>
+        public static bool IsPartOrAsm(AppModel appModel)
+        {
+            AppDocType type = appModel.DocType;
+            if (type == AppDocType.swASM || type == AppDocType.swPART)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Шаблон - является деталью или сборкой или компонентом
+        /// </summary>
+        /// <param name="appModel"></param>
+        /// <returns></returns>
+        public static bool IsPartOrAsmOrComp(AppModel appModel)
+        {
+            AppDocType type = appModel.DocType;
+            if (type == AppDocType.swASM ||
+                type == AppDocType.swPART ||
+                type == AppDocType.swCOMPONENT)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool StringPropertyConvert(string value, out string outvalue)
+        {
+            bool ret = false;
+            if (!String.IsNullOrEmpty(value))
+            {
+                ret = true;
+                outvalue = value;
+            }
+            else
+                outvalue = null;
+
+            return ret;
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// свойство модели
+    /// </summary>
+    public class AppBaseModelProp : ISwProperty
+    {
+
+        public AppModel AppModel
+        {
+            get => _appModel;
+            set
+            {
+                _appModel = value;
+                IsValid = Validator(_appModel);
+            }
+        }
+
+        public virtual bool IsReadable { get; set; }
+
+        public virtual bool IsWritable { get; set; }
+
+        public virtual string UserName { get; set; }
+        public virtual string PropertyName { get; set; }
+        public virtual bool IsValid { get; private set; }
+        public virtual string PropertyValue
+        {
+            get => _propertyValue;
+            set => _tempPropertyValue = value;
+        }
+
+        public virtual string ConfigName
+        {
+            get => _configName ?? ModelConfigProxy.GetActiveConfName(AppModel.SwModel);
+            set => _configName = value;
+        }
+
+        public PropValidator Validator = PropValidatorTemplate.IsPartOrAsmOrComp;
+
+        private AppModel _appModel;
+        private string _propertyValue;
+        private string _tempPropertyValue;
+        private string _configName = null;
+
+        public virtual void Update()
+        {
+            _tempPropertyValue = null;
+            _propertyValue = null;
+            if (IsValid && IsReadable)
+            {
+                _propertyValue = ModelConfigProxy.GetConfParam(
+                    AppModel.SwModel, ConfigName, PropertyName);
+            }
+        }
+
+        public virtual bool WriteValue()
+        {
+            bool ret = false;
+            if (CheckWrite())
+            {
+                ret = ModelConfigProxy.SetConfParam(AppModel.SwModel,
+                    ConfigName, PropertyName, _tempPropertyValue);
+            }
+            return ret;
+        }
+        /// <summary>
+        /// Проверка перед записью значения
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckWrite()
+        {
+            bool ret = false;
+            if (
+                IsWritable &&
+                !String.IsNullOrEmpty(_tempPropertyValue) &&
+                _tempPropertyValue != PropertyValue)
+            {
+                ret = true;
+            }
+
+            return ret;
+        }
+    }
+
+}
