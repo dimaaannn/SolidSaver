5f07ba0b25e883b8407dfe0aa9d721d0ac4aeb66
dimaaannn <Dk>
2020-12-14 17:41:31 +0300

    EOD

--- SWAPIlib/PropertyObj/PropertyChanger.cs
+++ SWAPIlib/PropertyObj/PropertyChanger.cs
@@ -18,8 +18,8 @@ namespace SWAPIlib.PropertyObj
         void ProceedValues();
         void WriteValues();
         void RestoreValues();
-        PropConstructor Propconstructor { get; set; }
-        ValueConverter Valueconverter { get; set; }
+
+        ValueConverter StringConv { get; set; }
         ObservableCollection<AppModel> Models { get; }
         ObservableCollection<ISwProperty> Properties { get; }
         bool AllConfigurations { get; set; }
@@ -66,56 +66,8 @@ namespace SWAPIlib.PropertyObj
 
     }
 
-    class PropertyChangerBase : IPropertyChanger
+    class PropertyChangerBase  //: IPropertyChanger
     {
-
-        public string SearchValue { get; set; }
-        public string NewValue { get ; set; }
-        public PropConstructor Propconstructor { get ;set; }
-        public ValueConverter Valueconverter { get; set ; }
-
-        public ObservableCollection<AppModel> Models => _Models;
-        ObservableCollection<AppModel> _Models = new ObservableCollection<AppModel>();
-
-        public ObservableCollection<ISwProperty> Properties => _Properties;
-        ObservableCollection<ISwProperty> _Properties = new ObservableCollection<ISwProperty>();
-
-        public PropertyChangerBase()
-        {
-            Models.CollectionChanged += Models_CollectionChanged;
-        }
-
-        private void Models_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
-            {
-                foreach (AppModel item in e.NewItems)
-                {
-                    //Properties.Add(Propconstructor(item));
-                }
-            }
-        }
-
-        public bool AllConfigurations { get; set; }
-
-        public void ProceedValues()
-        {
-            foreach (var prop in Properties)
-            {
-                if(prop.IsReadable && prop.IsWritable)
-                    prop.PropertyValue = Valueconverter(prop.PropertyValue);
-            }
-        }
-
-        public void RestoreValues()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void WriteValues()
-        {
-            throw new NotImplementedException();
-        }
 
         
     }
---
+++ SWAPIlib/PropertyObj/StringChangerUnit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAPIlib.PropertyObj
+{
+    public delegate IEnumerable<T> TextGetter<T, T1>(T1 obj);
+    public delegate bool TextSetter<T>(T obj, string setText);
+    public delegate bool TextReplacer(string input, string searchString, string replaceString, out string output);
+
+
+    public interface IStringChangerUnit<T, T1>
+    {
+        TextGetter<T, T1> textGetter { get; set; }
+        TextSetter<T1> textSetter { get; set; }
+        TextReplacer textReplacer { get; set; }
+        Dictionary<T, bool> ReplaceCollection { get; set; }
+        string SearchText { get; set; }
+        string ReplaceText { get; set; }
+        void Proceed();
+
+    }
+
+    class StringChangerUnit<ObjT, ItemT> : IStringChangerUnit<ObjT, ItemT> 
+    {
+        public TextGetter<ObjT, ItemT> textGetter { get ; set; }
+        public TextSetter<ObjT> textSetter { get ; set ; }
+        public TextReplacer textReplacer { get; set; }
+        public Dictionary<ObjT, bool> ReplaceCollection { get ; set; }
+        public string SearchText { get ; set; }
+        public string ReplaceText { get ; set; }
+
+        public void Proceed()
+        {
+            string input, output;
+            bool success;
+            foreach(var item in ReplaceCollection)
+            {
+                input = textGetter(item.Key);
+                success = textReplacer(input, SearchText, ReplaceText, out output);
+                if (success) 
+                    textSetter(item.Key, output);
+                ReplaceCollection[item.Key] = success;
+            }
+        }
+    }
+
+    public static class GetterColl
+    {
+        public static TextGetter<PropModifier> PropertyGetter = (PropModifier modifier) =>
+        {
+            modifier.
+        }
+    }
+}
