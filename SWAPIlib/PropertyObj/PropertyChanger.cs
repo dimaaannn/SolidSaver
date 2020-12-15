62c2e491b1bba4107d71bd855655876a0450f249
dimaaannn <Dk>
2020-12-15 17:37:30 +0300

    PropChanger - created interface and class

--- SWAPIlib/PropertyObj/PropertyChanger.cs
+++ SWAPIlib/PropertyObj/PropertyChanger.cs
@@ -15,14 +15,17 @@ namespace SWAPIlib.PropertyObj
     {
         string SearchValue { get; set; }
         string NewValue { get; set; }
+        bool UseRegExp { get; set; }
+        PropConstructor propConstructor {get;set;}
         void ProceedValues();
         void WriteValues();
         void RestoreValues();
 
-        ValueConverter StringConv { get; set; }
-        ObservableCollection<AppModel> Models { get; }
-        ObservableCollection<ISwProperty> Properties { get; }
+        ITextReplacer textReplacer { get; }
+        ObservableCollection<IAppComponent> Components { get; }
+        ObservableCollection<IPropModifier> Properties { get; }
         bool AllConfigurations { get; set; }
+        
     }
 
     public interface IPropModifier
@@ -66,9 +69,112 @@ namespace SWAPIlib.PropertyObj
 
     }
 
-    class PropertyChangerBase  //: IPropertyChanger
+    class PropertyChanger : IPropertyChanger
     {
+        public PropertyChanger()
+        {
+            textReplacer = new TextReplacer() { UseRegExp = false };
+            Components = new ObservableCollection<IAppComponent>();
+            Properties = new ObservableCollection<IPropModifier>();
+            Components.CollectionChanged += Components_AddRemoveItem;
+        }
 
-        
+
+        public string SearchValue 
+        { 
+            get => textReplacer.SearchText; 
+            set => textReplacer.SearchText = value; 
+        }
+        public string NewValue
+        {
+            get => textReplacer.ReplaceText;
+            set => textReplacer.ReplaceText = value;
+        }
+        public bool UseRegExp
+        {
+            get => textReplacer.UseRegExp;
+            set => textReplacer.UseRegExp = value;
+        }
+
+        public PropConstructor propConstructor { get; set; }
+        public ITextReplacer textReplacer { get; private set; }
+        public ObservableCollection<IAppComponent> Components { get; private set; }
+        public ObservableCollection<IPropModifier> Properties { get; private set; }
+
+        public bool AllConfigurations
+        {
+            get => _allConfigurations;
+            set
+            {
+                _allConfigurations = value;
+                foreach (var propModif in Properties)
+                {
+                    propModif.AllConfiguration = _allConfigurations;
+                }
+            }
+        }
+        private bool _allConfigurations = false;
+
+        public void ProceedValues()
+        {
+            var allProps = from propmod in Properties
+                           from prop in propmod.SwPropList
+                           select prop.Value;
+
+            foreach (var prop in allProps)
+            {
+                textReplacer.Replace(prop.PropertyValue);
+                if (textReplacer.IsReplaced)
+                    prop.PropertyValue = textReplacer.ReplaceResult;
+
+            }
+        }
+
+        public void RestoreValues()
+        {
+            foreach (var prop in Properties)
+            {
+                prop.RestoreValues();
+            }
+        }
+
+        public void WriteValues()
+        {
+            foreach (var prop in Properties)
+            {
+                prop.WriteValues();
+            }
+        }
+
+        protected void AddComponent(AppComponent component)
+        {
+            var newProp = new PropModifier(component.PartModel, propConstructor)
+            { AllConfiguration = this.AllConfigurations };
+
+            //Add current active configuration
+            newProp.ConfigNames.Add(component.RefConfigName);
+            Properties.Add(newProp);
+        }
+        protected void RemoveComponent(AppComponent component)
+        {
+            var removedProperty = Properties.Where(prop => prop.Model == component.PartModel);
+            foreach(var prop in removedProperty)
+            {
+                Properties.Remove(prop);
+            }
+        }
+        private void Components_AddRemoveItem(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                var newComp = e.NewItems[0] as AppComponent;
+                AddComponent(newComp);
+            }
+            else if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                RemoveComponent(e.OldItems[0] as AppComponent);
+            }
+        }
+
     }
 }
