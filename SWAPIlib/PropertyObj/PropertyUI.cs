96ec9c97dd0e124df1dfbccda775384e2d864f7d
dimaaannn <Dk>
2020-12-22 17:32:40 +0300

    Created PropertyUI wrapper - EOD

---
+++ SWAPIlib/PropertyObj/PropertyUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAPIlib.PropertyObj
+{
+    class PropertyUI
+    {
+        public PropertyChanger PropChanger { get; protected set; }
+        public Dictionary<string, PropConstructor> Constructors { get; set; }
+        public string ActiveConstructor { get; set; }
+        public PropConstructor GetConstructor();
+
+
+    }
+}
