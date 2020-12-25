20e495dd2e16469fe678c1e331f3758764b94d0d
dimaaannn <Dk>
2020-12-25 15:21:41 +0300

    Created ComponentControl class

--- SWAPIlib/Controller/Part/PartControl.cs
+++ SWAPIlib/Controller/Part/PartControl.cs
@@ -8,6 +8,7 @@ namespace SWAPIlib.Controller
 {
     public interface IPartControl<T> where T : ISwModel
     {
+        bool IsSelected { get; set; }
         /// <summary>
         /// Модель детали
         /// </summary>
@@ -24,34 +25,57 @@ namespace SWAPIlib.Controller
         /// Имя детали
         /// </summary>
         string Title { get; }
+    }
+
+    public interface IComponentControl : IPartControl<IAppComponent>
+    {
         /// <summary>
         /// Дочерние компоненты
         /// </summary>
-        List<IPartControl<IAppComponent>> SubComponents { get; }
+        List<IComponentControl> SubComponents { get; }
+        /// <summary>
+        /// Имя связанной конфигурации
+        /// </summary>
+        string RefConfig { get; }
     }
 
-    class PartControl<T> : IPartControl<T>
+    public class PartControl<T> : IPartControl<T>
         where T : ISwModel
     {
+        /// <summary>
+        /// PartControl constructor
+        /// </summary>
+        /// <param name="part"></param>
         public PartControl(T part)
         {
             Appmodel = part;
         }
+        public bool IsSelected { get; set; } = false;
         public T Appmodel { get; set; }
         public object Selector { get; }
         public AppDocType PartType => Appmodel.DocType;
         public string Title => Appmodel.Title;
-        List<IPartControl<IAppComponent>> _subComponents = null;
-        public List<IPartControl<IAppComponent>> SubComponents
+        public override string ToString()
+        {
+            return $"{Appmodel.FileName}:{PartType}";
+        }
+        
+    }
+
+    public class ComponentControl : PartControl<IAppComponent>, IComponentControl
+    {
+        public ComponentControl(IAppComponent component) : base(component) { }
+        List<IComponentControl> _subComponents = null;
+        public List<IComponentControl> SubComponents
         {
             get
             {
                 if (_subComponents != null)
                     return _subComponents;
 
-                _subComponents = new List<IPartControl<IAppComponent>>();
+                _subComponents = new List<IComponentControl>();
 
-                if(Appmodel is IAppComponent mainComponent)
+                if (Appmodel is IAppComponent mainComponent)
                 {
                     if (mainComponent.GetChildrenCount() <= 0)
                         return _subComponents;
@@ -59,7 +83,7 @@ namespace SWAPIlib.Controller
                     {
                         foreach (var component in mainComponent.GetComponents(true))
                         {
-                            _subComponents.Add(new PartControl<IAppComponent>(component));
+                            _subComponents.Add(new ComponentControl(component));
                         }
                     }
 
@@ -67,5 +91,12 @@ namespace SWAPIlib.Controller
                 return _subComponents;
             }
         }
+        public string RefConfig => Appmodel.RefConfigName;
+
+        public override string ToString()
+        {
+            return $"{Title}:{PartType}";
+        }
     }
+
 }
