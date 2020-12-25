dcb15f04e506951ec7271916d7b84fc5eb922731
dimaaannn <Dk>
2020-12-25 17:59:03 +0300

    Created GlobalOptions class

--- SWAPIlib/VisualInterface/InterfaceObjects.cs
+++ SWAPIlib/VisualInterface/InterfaceObjects.cs
@@ -10,6 +10,43 @@ using System.Collections.Specialized;
 
 namespace SWAPIlib
 {
+    public static class GlobalOptions
+    {
+        /// <summary>
+        /// Путь к основной открытой детали
+        /// </summary>
+        public static string MainPartPath
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_mainPartPath))
+                    _mainPartPath = ModelProxy.GetPathName(SwAppControl.MainModel);
+                return _mainPartPath;
+            }
+            set
+            {
+                _mainPartPath = value;
+            }
+        }
+        static string _mainPartPath;
+        /// <summary>
+        /// Рабочая папка сборки
+        /// </summary>
+        public static string ModelRootFolder
+        {
+            get => String.IsNullOrEmpty(_modelRootFolder) ?
+                System.IO.Path.GetDirectoryName(MainPartPath) :
+                _modelRootFolder;
+            set => _modelRootFolder = value;
+        }
+        static string _modelRootFolder;
+        /// <summary>
+        /// Рабочая папка проекта
+        /// </summary>
+        public static string MainFolder { get; set; } = ModelRootFolder;
+
+
+    }
 
     public class MainModel : VisualInterface.IMainModel
     {
