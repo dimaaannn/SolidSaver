68f32c3bddff91a0ce3de4231d95fb5ea1a14df2
dimaaannn <Dk>
2020-12-26 13:08:51 +0300

    Created Main control class

---
+++ SWAPIlib/Controller/MainModel/MainControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAPIlib.Controller.MainModel
+{
+    public interface IRootModel
+    {
+        /// <summary>
+        /// Коренная модель
+        /// </summary>
+        ISwModel RootModel { get; set; }
+        /// <summary>
+        /// Тип документа модели
+        /// </summary>
+        AppDocType DocType { get; }
+        /// <summary>
+        /// Имя документа
+        /// </summary>
+        string Title { get; }
+        /// <summary>
+        /// Имя активной конфигурации
+        /// </summary>
+        string ActiveConfigName { get; set; }
+
+    }
+}
