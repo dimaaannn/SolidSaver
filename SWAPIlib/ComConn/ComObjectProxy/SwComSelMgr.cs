568c2f6783eae66a612e023a6614aa72587e3d56
dimaaannn <Dk>
2021-01-18 15:36:41 +0300

    Add AppSelMgr (selection manager)

--- SWAPIlib/ComConn/ComObjectProxy/SwComSelMgr.cs
+++ SWAPIlib/ComConn/ComObjectProxy/SwComSelMgr.cs
@@ -6,13 +6,9 @@ using System.Threading.Tasks;
 using SolidWorks.Interop.sldworks;
 
 
-namespace SWAPIlib.ComConn.ComObjectProxy
+namespace SWAPIlib.ComConn
 {
-    public interface IAppSelMgr
-    {
-        ISelectionMgr SwSelMan { get; }
 
-    }
 
     public static class AppSelMgr
     {
@@ -33,11 +29,36 @@ namespace SWAPIlib.ComConn.ComObjectProxy
             }
         }
         private static SelectionMgr _swSelMan;
-        public static int SelectionGroup { get; set; } = -1;
+
+        /// <summary>
+        /// Объект свойств выделения SW
+        /// </summary>
+        public static SelectData SelData
+        {
+            get
+            {
+                if(_selData == null)
+                {
+                    _selData = SwSelMan.CreateSelectData();
+                    _selData.Mark = -1; //Default selection mark
+                }
+                return _selData;
+            }
+            set => _selData = value;
+        }
+        private static SelectData _selData;
+        /// <summary>
+        /// Маркировка выделения
+        /// </summary>
+        public static int SelectionMark
+        {
+            get => SelData.Mark;
+            set => SelData.Mark = value;
+        }
         /// <summary>
         /// Количество выделенных объектов
         /// </summary>
-        public static int CurrentSelectCount => SwSelMan.GetSelectedObjectCount2(SelectionGroup); //-1 - all models, 0 - only whithout mark
+        public static int CurrentSelectCount => SwSelMan.GetSelectedObjectCount2(SelectionMark); //-1 - all models, 0 - only whithout mark
         /// <summary>
         /// Выбрать компонент
         /// </summary>
@@ -46,15 +67,22 @@ namespace SWAPIlib.ComConn.ComObjectProxy
         /// <returns></returns>
         public static bool Select(Component2 component, bool Append=false)
         {
-            return component.Select4(Append, null, false);
+            return component.Select4(Append, SelData, false); //append, SelectData, show popup
         }
+        /// <summary>
+        /// Убрать выделение
+        /// </summary>
+        /// <param name="component"></param>
+        /// <returns></returns>
+        public static bool DeSelect(Component2 component) => component.DeSelect();
+        //public static bool Deselect(Component2 component) => SwSelMan.IDeSelect2()
         /// <summary>
         /// Возможность выделения деталей в SW
         /// </summary>
-        public static bool EnableSelection
+        public static bool DisableSelection
         {
-            get => SwSelMan.EnableSelection;
-            set => SwSelMan.EnableSelection = value;
+            get => !SwSelMan.EnableSelection;
+            set => SwSelMan.EnableSelection = !value;
         }
         /// <summary>
         /// Групповое выделение компонентов
@@ -67,7 +95,7 @@ namespace SWAPIlib.ComConn.ComObjectProxy
                 int selCount = CurrentSelectCount;
                 for (int i = 0; i < selCount; i++)
                 {
-                    ret[i] = SwSelMan.GetSelectedObjectsComponent3(i+1, SelectionGroup);
+                    ret[i] = SwSelMan.GetSelectedObjectsComponent3(i+1, SelectionMark);
                 }
                 return ret;
             }
@@ -75,7 +103,7 @@ namespace SWAPIlib.ComConn.ComObjectProxy
             {
                 foreach (var comp in value)
                 {
-                    Select(comp);
+                    Select(comp, true);
                 }
             }
         }
