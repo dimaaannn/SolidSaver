49f004719973ab06ab2dd0debc3c7009ac3871b5
dimaaannn <Dk>
2021-01-20 11:24:06 +0300

    Add DataConverter dictionary

---
+++ SolidSaverWPF/ResDictionary/DataConverters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SolidSaverWPF.ResDictionary
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+            if(value is bool boolval)
+            {
+                
+                //return System.Windows.UIElement.VisibilityProperty.
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
