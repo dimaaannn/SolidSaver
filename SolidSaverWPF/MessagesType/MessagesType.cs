6f5d19f01c45ec9ceae8abb2103d188d27f16207
dimaaannn <Dk>
2021-02-26 14:08:39 +0300

    Create DocLoaderView and DocLoaderViewModel
    Manage opened doc and send MVVM message on LoadButton pressed

---
+++ SolidSaverWPF/MessagesType/MessagesType.cs
@@ -0,0 +1,44 @@
+﻿using SWAPIlib.BaseTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidSaverWPF.MessagesType
+{
+    /// <summary>
+    /// Сообщение с моделью
+    /// </summary>
+    public class ModelMessage
+    {
+        public ModelMessage(ISwModelWrapper modelWrapper, ModelMessageAction action = ModelMessageAction.None)
+        {
+            SwModel = modelWrapper;
+            Action = action;
+        }
+        private ModelMessageAction _Action;
+        public ModelMessageAction Action
+        {
+            get { return _Action; }
+            set { _Action = value; }
+        }
+
+        private ISwModelWrapper _SwModel;
+        /// <summary>
+        /// Модель
+        /// </summary>
+        public ISwModelWrapper SwModel
+        {
+            get { return _SwModel; }
+            set { _SwModel = value; }
+        }
+
+    }
+
+    public enum ModelMessageAction
+    {
+        None,
+        SetAsMainModel
+    }
+}
---
+++ SolidSaverWPF/ViewModel/DocLoaderViewModel.cs
@@ -0,0 +1,75 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using SolidSaverWPF.MessagesType;
+using SWAPIlib.BaseTypes;
+using SWAPIlib.ComConn;
+using SWAPIlib.Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SolidSaverWPF.ViewModel
+{
+    public class DocLoaderViewModel : ViewModelBase
+    {
+
+        private List<ISwModelWrapper> _OpenedModels;
+        /// <summary>
+        /// Список открытых моделей
+        /// </summary>
+        public List<ISwModelWrapper> OpenedModels {
+            get
+            {
+                if (_OpenedModels is null)
+                    UpdateList();
+                return _OpenedModels;
+            }
+            private set => Set(ref _OpenedModels, value); }
+
+        private int _SelectedIndex;
+        public int SelectedIndex { get => _SelectedIndex;
+            set => Set(ref _SelectedIndex, value);
+        }
+
+
+        #region UpdateCommand
+        /// <summary>
+        /// Загрузить список видимых документов
+        /// </summary>
+        public ICommand UpdateListCommand => _UpdateListCommand ?? (_UpdateListCommand = new RelayCommand(UpdateList, UpdateListCommandCanExecute));
+        private ICommand _UpdateListCommand;
+        private bool UpdateListCommandCanExecute() => SwAppControl.ComConnected;
+        #endregion
+
+        #region LoadDocumentCommand
+        /// <summary>
+        /// Загрузить выбранный документ
+        /// </summary>
+        public ICommand LoadDocumentCommand => _LoadDocumentCommand ?? (_LoadDocumentCommand = new RelayCommand(UpdateList, LoadDocumentCommandCanExecute));
+        private ICommand _LoadDocumentCommand;
+        private bool LoadDocumentCommandCanExecute()
+        {
+            return SwAppControl.ComConnected
+                && SelectedIndex >= 0
+                && SelectedIndex < OpenedModels.Count;
+        }
+        #endregion
+
+
+        public void UpdateList()
+        {
+            OpenedModels = OpenedDocs.GetVisibleDocs();
+        }
+
+        /// <summary>
+        /// Сообщение об установке в качестве главной модели
+        /// </summary>
+        public void LoadSelected()
+        {
+            MessengerInstance.Send(new ModelMessage(OpenedModels[SelectedIndex], ModelMessageAction.SetAsMainModel));
+        }
+    }
+}
