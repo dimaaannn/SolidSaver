bddd2eec1017a0422ccc39a5e7b1cb8fe6a7c48c
dimaaannn <Dk>
2021-02-26 11:39:55 +0300

    Implement SwModelWrapper (lightweight wrapper for model)

--- SWAPIlib/Global/LinkedModel.cs
+++ SWAPIlib/Global/LinkedModel.cs
@@ -9,6 +9,7 @@ using SWAPIlib.Controller;
 using System.Collections;
 using System.Collections.ObjectModel;
 using SolidWorks.Interop.sldworks;
+using System.IO;
 
 namespace SWAPIlib.Global
 {
@@ -227,6 +228,57 @@ namespace SWAPIlib.Global
 
         public event EventHandler<SwEventArgs> CloseRootModel;
         public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    /// <summary>
+    /// Легковесная обёртка над классом модели
+    /// </summary>
+    public interface ISwModelWrapper
+    {
+        /// <summary>
+        /// Имя файла
+        /// </summary>
+        string FileName { get; }
+        /// <summary>
+        /// Полный путь к файлу
+        /// </summary>
+        string FilePath { get; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        string Title { get; }
+        /// <summary>
+        /// Модель привязки
+        /// </summary>
+        ModelDoc2 SwModel { get; set; }
+        /// <summary>
+        /// Тип документа
+        /// </summary>
+        AppDocType DocType { get; }
+        /// <summary>
+        /// Сгенерировать класс приложения
+        /// </summary>
+        IAppModel GetAppModel();
+    }
+
+    /// <summary>
+    /// Класс обёртка над моделью
+    /// </summary>
+    public class SwModelWrapper : ISwModelWrapper
+    {
+        public SwModelWrapper() { }
+        public SwModelWrapper(ModelDoc2 model) : this()
+        {
+            _SwModel = model;
+        }
+        private ModelDoc2 _SwModel;
+        public ModelDoc2 SwModel { get => _SwModel; set => _SwModel = value; }
+
+        public string FilePath => ModelProxy.GetPathName(SwModel);
+        public string FileName => Path.GetFileName(FilePath);
+        public string Title => ModelProxy.GetName(SwModel);
+        public AppDocType DocType => PartTypeChecker.GetSWType(SwModel);
+        public IAppModel GetAppModel() => ModelClassFactory.GetModel(SwModel);
     }
 
 }
