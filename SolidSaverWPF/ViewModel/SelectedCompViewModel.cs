62315f4cb3844cd56bbb3b480b73b63d7dcbd18c
dimaaannn <Dk>
2021-03-09 15:22:46 +0300

    ComponentInfoView, SelectedCompView - done

---
+++ SolidSaverWPF/ViewModel/SelectedCompViewModel.cs
@@ -0,0 +1,69 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using SolidSaverWPF.Data;
+using SolidSaverWPF.MessagesType;
+using SWAPIlib.BaseTypes;
+using SWAPIlib.ComConn;
+using SWAPIlib.Controller;
+using SWAPIlib.Global;
+using SWAPIlib.MProperty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SolidSaverWPF.ViewModel
+{
+    /// <summary>
+    /// Отображает свойства выбранного пользователем компонента
+    /// </summary>
+    public class SelectedCompViewModel : ViewModelBase
+    {
+        public SelectedCompViewModel()
+        {
+            MainModel.MainModelChanged += ClearData;
+            MessengerInstance.Register<SelectionMessage<object>>(this, ComponentSelected);
+        }
+
+
+        private IComponentControl componentControl;
+        /// <summary>
+        /// Выбранный компонент
+        /// </summary>
+        public IComponentControl SelectedComponent { get => componentControl; set => Set(ref componentControl, value); }
+
+        /// <summary>
+        /// Список свойств
+        /// </summary>
+        public List<IPropertyModel> PropertyModel => SelectedComponent.PartModel.PropList;
+
+        /// <summary>
+        /// Компонент выбран пользователем
+        /// </summary>
+        /// <param name="obj"></param>
+        private void ComponentSelected(SelectionMessage<object> obj)
+        {
+            switch (obj.Selection)
+            {
+                case IComponentControl comp:
+                    SelectedComponent = comp;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Очистить список
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearData(object sender, EventArgs e)
+        {
+            SelectedComponent = null;
+        }
+
+    }
+}
