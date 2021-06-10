1156e969deab592c3fb9f23f524c06c2423ff256
dimaaannn <Dk>
2021-06-10 12:02:51 +0300

    WPF Test - create JustDataView

---
+++ tests/TestWPF/BuissnesLogic/SampleData.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPF.BuissnesLogic
+{
+    public class JustData : ViewModelBase
+    {
+        private int iD = -1;
+        private string name = "default";
+        private int age = 0;
+
+        public string Name { get => name; set => Set(ref name, value); }
+        public int Age { get => age; set => Set(ref age, value); }
+        public int ID { get => iD; protected set => Set(ref iD, value); }
+    }
+}
