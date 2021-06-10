2277afb7540ed8ed186ca096d08f2f6e9a2cee64
dimaaannn <Dk>
2021-06-10 17:28:24 +0300

    EOD: WPF test with style

--- tests/TestWPF/BuissnesLogic/SampleData.cs
+++ tests/TestWPF/BuissnesLogic/SampleData.cs
@@ -9,12 +9,35 @@ namespace TestWPF.BuissnesLogic
 {
     public class JustData : ViewModelBase
     {
+        
         private int iD = -1;
         private string name = "default";
         private int age = 0;
 
+        public JustData() { }
+        public JustData(int i) : base()
+        {
+            iD = i;
+        }
+
         public string Name { get => name; set => Set(ref name, value); }
         public int Age { get => age; set => Set(ref age, value); }
         public int ID { get => iD; protected set => Set(ref iD, value); }
+    }
+
+    public class DataProvider
+    {
+        public static List<JustData> GetData() => GetData(14);
+        public static List<JustData> GetData(int i)
+        {
+            var ret = new List<JustData>();
+            var r = Enumerable.Range(0, i);
+            ret.AddRange(
+                r
+                .Select(num => 
+                    new JustData(num) { Name = $"Data-{num}" }
+                ).ToList());
+            return ret;
+        }
     }
 }
