f560fcb7629fa7ba1657603da2ffdc4da3e76ece
dimaaannn <Dk>
2021-06-11 12:05:23 +0300

    ICell interface

---
+++ SWAPIlib/Table/ICell.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAPIlib.Table
+{
+    public interface ICell : INotifyPropertyChanged
+    {
+        string Text { get; }
+        void Update();
+    }
+
+    public interface IWritableCell : ICell
+    {
+        string TempText { get; set; }
+        bool WriteValue();
+    }
+
+    public interface ITableCell : ICell
+    {
+        ITable RefTable { get; set; }
+    }
+
+    public abstract class BaseCell : ICell
+    {
+        private string text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public string Text { get => text; protected set { OnPropertyChanged("Text"); text = value; } }
+        public abstract void Update();
+
+    }
+
+    public class TextCell : BaseCell, IWritableCell
+    {
+        private string tempText;
+
+        public string TempText { get => tempText; set { OnPropertyChanged(); tempText = value; } }
+
+        public override void Update()
+        {
+            TempText = null;
+        }
+
+        public bool WriteValue()
+        {
+            Text = TempText;
+            return true;
+        }
+    }
+}
---
+++ SWAPIlib/Table/ITable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SWAPIlib.Table
+{
+    public interface ITable : IEnumerable<KeyValuePair<string, ICell>>
+    {
+        ICell GetCell(string cellKey);
+    }
+
+    public interface ITargetTable : ITable
+    {
+        SWAPIlib.Property.ITarget GetTarget();
+    }
+
+
+    public abstract class BaseTable : ITable
+    {
+        protected Dictionary<string, ICell> Cells = new Dictionary<string, ICell>();
+
+        public virtual ICell GetCell(string cellKey)
+        {
+            ICell ret;
+            Cells.TryGetValue(cellKey, out ret);
+            return ret;
+        }
+
+        public IEnumerator<KeyValuePair<string, ICell>> GetEnumerator()
+        {
+            return Cells.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
