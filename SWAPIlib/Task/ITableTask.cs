01b24866379ec84e2d58e0bee961a6a09d9a541e
dimaaannn <Dk>
2021-06-17 12:49:48 +0300

    Add ITableTask class

---
+++ SWAPIlib/Task/ITableTask.cs
@@ -0,0 +1,31 @@
+﻿using SWAPIlib.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAPIlib.Task
+{
+    public interface ITableTask : ITableAction, ITableChecker
+    {
+
+    }
+
+    public class TableTask : ITableTask
+    {
+        public TableTask(TableActionDelegate action)
+        {
+            if (action == null)
+                throw new NullReferenceException("TableTask: Null action ref");
+            ProceedAction = action;
+        }
+
+        public string Name { get; set; }
+        public CheckTableDelegate CheckTable { get; set; }
+        protected TableActionDelegate ProceedAction { get; set; }
+
+        public TableLog Proceed(ref ITable refTable, ITable settings) => 
+            ProceedAction(refTable: ref refTable, settings: settings);
+    }
+}
