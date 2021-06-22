19535616cdf3fd7828417f6aa7a3b29497941030
dimaaannn <Dk>
2021-06-22 12:08:57 +0300

    CellView - bind double click to copy text, clear codeBehind

--- SolidSaverWPF/Views/Table/CellView.xaml.cs
+++ SolidSaverWPF/Views/Table/CellView.xaml.cs
@@ -25,58 +25,5 @@ namespace SolidSaverWPF.Views.Table
         {
             InitializeComponent();
         }
-
-
-        private void PropCurrentValue_GotFocus(object sender, RoutedEventArgs e)
-        {
-            var brush = new SolidColorBrush(Colors.White) { Opacity = 0.7 };
-            PropCurrentValue.Background = brush;
-        }
-
-        private void PropCurrentValue_LostFocus(object sender, RoutedEventArgs e)
-        {
-            PropCurrentValue.Background = Brushes.Transparent;
-        }
-
-    }
-
-    public class NullToColorConverter : IValueConverter
-    {
-
-        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            bool status = String.IsNullOrEmpty((string)value);
-            if (!status)
-            {
-                return new SolidColorBrush(Colors.LightYellow);
-            }
-            else return new SolidColorBrush(Colors.White);
-        }
-
-        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    /// <summary>
-    /// После изменения текста сделать записанное значение серым
-    /// </summary>
-    public class ValueTextColorConverter : IValueConverter
-    {
-        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            bool notNull = !String.IsNullOrEmpty((string)value);
-            if (notNull)
-            {
-                return new SolidColorBrush(Colors.LightGray);
-            }
-            else return new SolidColorBrush(Colors.Black);
-        }
-
-        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
