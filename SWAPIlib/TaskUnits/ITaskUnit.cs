d456d07e055da297a00690320f515053abbf2869
dimaaannn <Dk>
2021-06-25 16:01:49 +0300

    Add FactoryProvider class and extension

---
+++ SWAPIlib/TaskUnits/ITaskUnit.cs
@@ -0,0 +1,17 @@
+﻿using SWAPIlib.Table;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAPIlib.TaskUnits
+{
+    public interface ITaskUnit
+    {
+        ITable GetSettings(ITable refTable);
+        bool Proceed(ref ITable refTable);
+
+        IFactoryProvider FactoryProvider { get; }
+    }
+
+}
