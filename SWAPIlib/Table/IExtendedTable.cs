70e85446ff05b8bd9cb7784f17cab0656f2b1dbd
dimaaannn <Dk>
2021-07-06 11:50:51 +0300

    Implement ExtendedTable class

---
+++ SWAPIlib/Table/IExtendedTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SWAPIlib.Table
+{
+    /// <summary>
+    /// Расширенный функционал таблиц с поддержкой объекта привязки
+    /// </summary>
+    public interface IExtendedTable : ITargetTable
+    {
+        ITargetWrapper Target { get; set; }
+
+    }
+
+    
+    public class ExtendedTable : BaseTable, IExtendedTable
+    {
+        public ITargetWrapper Target { get; set; }
+
+        public object GetTarget() => Target.GetTarget();
+    }
+}
