4d98f71a4dae5097aa66ced4603073cdb357e309
dimaaannn <Dk>
2021-07-09 12:18:14 +0300

    ActionUnit class imlement

--- SWAPIlib/TaskCollection/IActionUnit.cs
+++ SWAPIlib/TaskCollection/IActionUnit.cs
@@ -6,21 +6,112 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SWAPIlib.TaskCollection
 {
-
-
-    public interface IActionUnit
+    /// <summary>
+    /// Информация для пользователя
+    /// </summary>
+    public interface IUserInfo
     {
-        IActionUnitResult Proceed(IExtendedTable table);
+        string Name { get; }
+        string InfoText { get; }
+        string NextButtonText { get; }
     }
 
+    /// <summary>
+    /// Обработчик задач для таблиц
+    /// </summary>
+    public interface IActionUnit : IObservable<IActionUnitResult>
+    {
+        bool IsCanExecute { get; }
+        IUserInfo UserInfo { get; }
+        IActionUnit NextAction { get; }
+        void Run(IExtendedTable table);
+    }
+
+    public class ActionUnit : IActionUnit
+    {
+        private readonly Subject<IActionUnitResult> tableProcessedSubject = new Subject<IActionUnitResult>();
+        public bool IsCanExecute => ActionList?.Count() > 0;
+        public IActionList ActionList { get; set; }
+        public IUserInfo UserInfo { get; set; }
+        public IActionUnit NextAction { get; set; }
+        public IDisposable Subscribe(IObserver<IActionUnitResult> observer) => tableProcessedSubject.Subscribe(observer);
+
+        public void Run(IExtendedTable table)
+        {
+            var result = ResultGenerator(table, ActionList.Proceed(table));
+            tableProcessedSubject.OnNext(result);
+        }
+
+
+        private IActionUnitResult ResultGenerator(IExtendedTable table, TableLog tableLog)
+        {
+            ActionUnitStatus status = tableLog.Status != LogStatus.Failed ? ActionUnitStatus.Completed : ActionUnitStatus.Failed;
+            return new ActionUnitResult {
+                ActionUnit = this,
+                TargetTable = table, 
+                ActionLog = tableLog };
+        }
+    }
+
+    public enum ActionUnitStatus
+    {
+        None,
+        Failed,
+        Passed,
+        Completed
+    }
     public interface IActionUnitResult
     {
+        ActionUnit ActionUnit { get; }
+        ActionUnitStatus Status { get; }
+        IExtendedTable TargetTable { get; }
+        TableLog ActionLog { get; }
     }
+
+    public class ActionUnitResult : IActionUnitResult
+    {
+        public ActionUnit ActionUnit { get; set; }
+        public ActionUnitStatus Status
+        {
+            get
+            {
+                ActionUnitStatus ret;
+                switch (ActionLog.Status)
+                {
+                    case LogStatus.None:
+                        ret = ActionUnitStatus.None;
+                        break;
+                    case LogStatus.Processed:
+                        ret = ActionUnitStatus.Completed;
+                        break;
+                    case LogStatus.Passed:
+                        ret = ActionUnitStatus.Passed;
+                        break;
+                    case LogStatus.Failed:
+                        ret = ActionUnitStatus.Failed;
+                        break;
+                    default:
+                        ret = ActionUnitStatus.None;
+                        break;
+                }
+                return ret;
+            }
+        }
+        public IExtendedTable TargetTable { get; set; }
+        public TableLog ActionLog { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ActionUnit?.UserInfo.Name}-\"{TargetTable?.Name}\": {Status}";
+        }
+    }
+
 
 
 
