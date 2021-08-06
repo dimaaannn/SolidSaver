5d933d87b4ce747bd94f03cd35277829a1918ec1
dimaaannn <Dk>
2021-08-06 16:04:01 +0300

    Move class TableProviderTemp to separated file

--- SWAPIlib/TaskCollection/IActionSceriano.cs
+++ SWAPIlib/TaskCollection/IActionSceriano.cs
@@ -1,5 +1,4 @@
-﻿using Ninject;
-using SWAPIlib.BaseTypes;
+﻿using SWAPIlib.BaseTypes;
 using SWAPIlib.Table;
 using SWAPIlib.TaskUnits;
 using System;
@@ -64,38 +63,6 @@ namespace SWAPIlib.TaskCollection
 
 
 
-    }
-
-    /// <summary>
-    /// Temp class for run test
-    /// </summary>
-    public class TableProviderTemp //TODO refactor provider
-    {
-        public TestAction TestAction { get; }
-        public TableProviderTemp()
-        {
-            //Initialiser.kernel.Bind<TestAction>().ToSelf().InSingletonScope();
-            TestAction = Initialiser.kernel.Get<TestAction>();
-        }
-        public ITableCollection UserSelectedModels()
-        {
-            var taskServices = Initialiser.kernel.Get<ITaskServices>();
-            var partProvider = taskServices.CreateSelectedModelProvider();
-
-            var tableProvider = taskServices.CreateTableProvider(partProvider);
-
-            return tableProvider.GetTable();
-        }
-
-
-        public IActionUnit GetActionUnit()
-        {
-            //Вернуть
-            IActionUnit ret = TestAction.GetGlobalInfoUnit();
-
-            return ret;
-
-        }
     }
 
 
---
+++ SWAPIlib/TaskCollection/TableProviderTemp.cs
@@ -0,0 +1,38 @@
+﻿using Ninject;
+
+namespace SWAPIlib.TaskCollection
+{
+    /// <summary>
+    /// Temp class for run test
+    /// </summary>
+    public class TableProviderTemp //TODO refactor provider
+    {
+        public TestAction TestAction { get; }
+        public TableProviderTemp()
+        {
+            //Initialiser.kernel.Bind<TestAction>().ToSelf().InSingletonScope();
+            TestAction = Initialiser.kernel.Get<TestAction>();
+        }
+        public ITableCollection UserSelectedModels()
+        {
+            var taskServices = Initialiser.kernel.Get<ITaskServices>();
+            var partProvider = taskServices.CreateSelectedModelProvider();
+
+            var tableProvider = taskServices.CreateTableProvider(partProvider);
+
+            return tableProvider.GetTable();
+        }
+
+
+        public IActionUnit GetActionUnit()
+        {
+            //Вернуть
+            IActionUnit ret = TestAction.GetGlobalInfoUnit();
+
+            return ret;
+
+        }
+    }
+
+
+}
